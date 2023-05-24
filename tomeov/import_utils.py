2deb7c904a7d42afbfe4b995e185b6ce878afe88
sawradip <sawradip0@gmail.com>
2023-05-24 08:15:15 +0100

    Adde import utils

---
+++ tomeov/import_utils.py
@@ -0,0 +1,21 @@
+def is_diffusers_available():
+    try:
+        import stable_diffusion
+        import utils
+        return True
+    except ImportError:
+        return False
+
+def is_openclip_available():
+    try:
+        import openclip
+        return True
+    except ImportError:
+        return False
+
+def is_timm_available():
+    try:
+        import timm
+        return True
+    except ImportError:
+        return False
